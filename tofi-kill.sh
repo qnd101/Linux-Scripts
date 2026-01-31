d624c5e76a311499de45c8c1b3287ddb8ae1230e
Yeonwoo Lee <leeywqnd@gmail.com>
2026-02-01 04:44:18 +0900

    Add RAM usage entry to tofi-kill.sh

--- tofi-kill.sh
+++ tofi-kill.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Fetch the list once; $1=PID, $2=PPID, $3=START, $4=COMMAND
-selected="$(ps -u "$USER" -o pid,ppid,bsdstart,comm --no-headers | \
-    awk '{printf "%s  %s  %-20s <%s\n", $1, $3, $4, $2}' | \
-    tofi --prompt-text "Kill: ")"
+selected="$(ps -u "$USER" -o pid,ppid,bsdstart,comm,rss --no-headers | \
+    awk '{printf "%s  %s  %6.1fM  %-20s \%s\n", $1, $3, $5/1024, $4, $2}' | \
+    tofi --width 650 --prompt-text "Kill: ")"
 
 if [[ -n $selected ]]; then
     # Extract just the PID (first column) and Command (third column) for the prompt
@@ -13,6 +13,6 @@ if [[ -n $selected ]]; then
     answer="$(echo -e "No\nYes" | tofi --prompt-text "Kill $selcomm ($selpid)? ")"
 
     if [[ $answer == "Yes" ]]; then
-        kill -9 "$selpid"
+        kill -15 "$selpid"
     fi
 fi
